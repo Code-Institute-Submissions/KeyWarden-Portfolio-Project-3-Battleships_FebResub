92334bfedf7702fdc7919f0d042be7d599430d3d
KeyWarden <ekeeley9@gmail.com>
2022-12-15 11:49:13 +0000

    menu valid and ai diff functions

--- run.py
+++ run.py
@@ -17,10 +17,39 @@ leaderboard = SHEET.worksheet('board')
 
 data = leaderboard.get_all_values()
 
-#declare contants to be used for input verification
-VALID_MENU_INPUT = ["1", "s", "start", "2", "o", "options", "3", "l", "leaderboard"]
-VALID_OPTIONS_INPUT = ["1", "d", "difficulty", "2", "m", "map", "3", "b", "back"]
-VALID_AI_INPUT = ["1", "e", "easy", "2", "n", "normal", "3", "h", "hard", "4", "b", "back"]
-VALID_MAP_SIZE_INPUT = ["1", "s", "small", "2", "m", "medium", "3", "l", "large", "4", "b", "back"]
-VALID_LEADERBOARD_INPUT = ["1", "b", "back"]
-VALID_SHIP_INPUT = ["1", "p", "place", "2", "r", "rotate"]
+# declare contants to be used for input verification
+MENU_INPUT = ["1", "s", "start", "2", "o", "options", "3", "l", "leaderboard"]
+OPTIONS_INPUT = ["1", "d", "difficulty", "2", "m", "map", "3", "b", "back"]
+AI_INPUT = ["1", "e", "easy", "2", "n", "normal", "3", "h", "hard", "4", "b", "back"]
+MAP_SIZE_INPUT = ["1", "s", "small", "2", "m", "medium", "3", "l", "large", "4", "b", "back"]
+LEADERBOARD_INPUT = ["1", "b", "back"]
+SHIP_INPUT = ["1", "p", "place", "2", "r", "rotate"]
+
+def valid_menu_input(choice):
+    # function to check validity of input on menu screen
+    n = 0
+    is_valid = False
+    while n < 9:
+        if choice == MENU_INPUT[n]:
+            is_valid = True
+            n = 9
+        else:
+            n += 1
+    return is_valid
+
+def ai_difficulty(difficulty):
+    if difficulty == 0:
+        return "Easy"
+    elif difficulty == 1:
+        return "Normal"
+    elif difficulty == 2:
+        return "Hard"
+
+def menu_screen(difficulty, size):
+    # function that controls menu screen
+    ai = "Easy"
+    map = "Small"
+
+    print("BATTLESHIPS")
+    print(f"Current Difficulty: {difficulty} - Current Map Size: ")
+    choice = input("Please enter your choice here: ")
