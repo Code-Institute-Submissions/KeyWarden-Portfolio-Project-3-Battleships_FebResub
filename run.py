d5b937e9fcdedb95c61672965398b28f837401fd
KeyWarden <ekeeley9@gmail.com>
2022-12-15 13:04:10 +0000

    fix multiple syntax issues

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# code needed to connect game to leaderboard in Google Sheets
+"""code needed to connect game to leaderboard in Google Sheets"""
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -17,53 +17,70 @@ leaderboard = SHEET.worksheet('board')
 
 data = leaderboard.get_all_values()
 
-# declare contants to be used for input verification
+# declare constants to be used for input verification
 MENU_INPUT = ["1", "s", "start", "2", "o", "options", "3", "l", "leaderboard"]
 OPTIONS_INPUT = ["1", "d", "difficulty", "2", "m", "map", "3", "b", "back"]
-AI_INPUT = ["1", "e", "easy", "2", "n", "normal", "3", "h", "hard", "4", "b", "back"]
-MAP_SIZE_INPUT = ["1", "s", "small", "2", "m", "medium", "3", "l", "large", "4", "b", "back"]
+AI_INPUT = [
+    "1", "e", "easy", "2", "n", "normal", "3", "h", "hard", "4", "b", "back"
+    ]
+MAP_SIZE_INPUT = [
+    "1", "s", "small", "2", "m", "medium", "3", "l", "large", "4", "b", "back"
+    ]
 LEADERBOARD_INPUT = ["1", "b", "back"]
 SHIP_INPUT = ["1", "p", "place", "2", "r", "rotate"]
 
+
 def valid_menu_input(choice):
     """function to check validity of input on menu screen"""
-    n = 0
+
+    num = 0
     is_valid = False
-    while n < 9:
-        if choice == MENU_INPUT[n]:
+    while num < 9:
+        if choice == MENU_INPUT[num]:
             is_valid = True
-            n = 9
+            num = 9
         else:
-            n += 1
+            num += 1
     return is_valid
+
 
 def ai_difficulty(difficulty):
     """function translates difficulty into text for user"""
+    result = ""
     if difficulty == 0:
-        return "Easy"
+        result = "Easy"
     elif difficulty == 1:
-        return "Normal"
+        result = "Normal"
     elif difficulty == 2:
-        return "Hard"
+        result = "Hard"
+    return result
+
 
 def map_size(size):
     """function translates map size into text for user"""
+    result = ""
     if size == 0:
-        return "Small"
+        result = "Small"
     elif size == 1:
-        return "Medium"
+        result = "Medium"
     elif size == 2:
-        return "Large"
+        result = "Large"
+    return result
+
 
 def start_game(difficulty, size):
+    """funtion that controls the game starting"""
     print("placeholder")
+
 
 def menu_output(validity, choice, difficulty, size):
     """funtion controls result of user input on menu screen"""
     if not validity:
-        print(f"Invalid input, please input one of the following: {MENU_INPUT}")
+        print(
+            f"Invalid input, please input one of the following: {MENU_INPUT}"
+            )
         menu_screen(difficulty, size)
-    
+
     if choice == "1" or choice == "s" or choice == "start":
         start_game(difficulty, size)
     elif choice == "2" or choice == "o" or choice == "options":
@@ -71,13 +88,14 @@ def menu_output(validity, choice, difficulty, size):
     elif choice == "3" or choice == "l" or choice == "leaderboard":
         leaderboard_screen(difficulty, size)
 
+
 def menu_screen(difficulty, size):
     """function that controls menu screen"""
-    ai = ai_difficulty(difficulty)
-    map = map_size(size)
+    a_i = ai_difficulty(difficulty)
+    game_map = map_size(size)
 
     print("BATTLESHIPS")
-    print(f"Current Difficulty: {ai} - Current Map Size: {map}")
+    print(f"Current Difficulty: {a_i} - Current Map Size: {game_map}")
     print("1. [S]tart")
     print("2. [O]ptions")
     print("3. [L]eaderboard")
@@ -85,8 +103,41 @@ def menu_screen(difficulty, size):
     validity = valid_menu_input(choice)
     menu_output(validity, choice, difficulty, size)
 
-def options_screen(difficulty, size):
+
+def valid_options_input(choice):
+    """function to check validity of input on options screen"""
+
+    num = 0
+    is_valid = False
+    while num < 9:
+        if choice == OPTIONS_INPUT[num]:
+            is_valid = True
+            num = 9
+        else:
+            num += 1
+    return is_valid
+
+
+def options_output(validity, choice, difficulty, size):
     print("placeholder")
 
+
+def options_screen(difficulty, size):
+    """funtion that controls the options screen"""
+    a_i = ai_difficulty(difficulty)
+    game_map = map_size(size)
+    print("OPTIONS")
+    print(f"Current Difficulty: {a_i} - Current Map Size: {game_map}")
+    print("1. [D]ifficulty")
+    print("2. [M]ap")
+    print("3. [B]ack")
+    choice = input("Please enter your choice here: ").lower()
+    validity = valid_options_input(choice)
+
+
 def leaderboard_screen(difficulty, size):
+    """function that controls the leaderboard screen"""
     print("placeholder")
+
+
+menu_screen(0, 0)
